ccf429f9496c9ba9d781dd1f4a7f8425e9245d5a
kurt dudek <kpdudek@bu.edu>
2018-07-07 18:05:23 -0400

    new runs

--- mandelbrot_set.py
+++ mandelbrot_set.py
@@ -1,10 +1,18 @@
+#!/usr/bin/python
+# coding: utf-8
+
+#Based on the code written by Jean-François Puget
+#https://gist.github.com/jfpuget
+
+
 import numpy as np
-#from numba import jit
+from numba import jit
 from matplotlib import pyplot as plt
 from matplotlib import colors
-#matplotlib inline
 
 
+@jit
+#Mathematical definition of mandelbrodt set
 def mandelbrot(z,maxiter,horizon,log_horizon):
     c = z
     for n in range(maxiter):
@@ -14,6 +22,9 @@ def mandelbrot(z,maxiter,horizon,log_horizon):
         z = z*z + c
     return 0
 
+
+@jit
+#Calculates the escape conditions for all pixels in the image
 def mandelbrot_set(xmin,xmax,ymin,ymax,width,height,maxiter):
     horizon = 2.0 ** 40
     log_horizon = np.log(np.log(horizon))/np.log(2)
@@ -25,8 +36,9 @@ def mandelbrot_set(xmin,xmax,ymin,ymax,width,height,maxiter):
             n3[i,j] = mandelbrot(r1[i] + 1j*r2[j],maxiter,horizon, log_horizon)
     return (r1,r2,n3)
 
-image_counter = 30
 
+#Saves the image with given filename
+image_counter = 7
 def save_image(fig):
     global image_counter
     filename = "mandelbrodt_%d.png" % image_counter
@@ -35,14 +47,15 @@ def save_image(fig):
 
 
 
-def mandelbrot_image(xmin,xmax,ymin,ymax,width=10,height=10,maxiter=80,cmap='jet',gamma=0.3):
-    dpi = 72
+#Produces the computer image of the mandelbrodt set based on given conditions
+def mandelbrot_image(xmin,xmax,ymin,ymax,width=20,height=20,maxiter=18000,cmap='jet',gamma=0.3):
+    dpi = 600
     img_width = dpi * width
     img_height = dpi * height
     x,y,z = mandelbrot_set(xmin,xmax,ymin,ymax,img_width,img_height,maxiter)
     
     fig, ax = plt.subplots(figsize=(width, height),dpi=72)
-    ticks = np.arange(0,img_width,3*dpi)
+    ticks = np.arange(0,img_width,dpi)
     x_ticks = xmin + (xmax-xmin)*ticks/img_width
     plt.xticks(ticks, x_ticks)
     y_ticks = ymin + (ymax-ymin)*ticks/img_width
@@ -56,13 +69,42 @@ def mandelbrot_image(xmin,xmax,ymin,ymax,width=10,height=10,maxiter=80,cmap='jet
 
 
 
+###Usage###
+
+
+
 #mandelbrot_image(-2.0,0.5,-1.25,1.25,cmap='hot')
 
+#x = np.linspace(-.8,-.75,.001)
+#y = np.linspace(.02525,.0256,.000007)
+
+#for i in range(0,50):
+#	mandelbrot_image(x[i],{2},{3},{4},cmap='hot')
+
+
+mandelbrot_image(-0.75125,-0.75105,.02525,.0256,cmap='hot')
+
+#mandelbrot_image(-0.85,-0.8,0,0.05,cmap='hot')
+
+#mandelbrot_image(-0.8,-0.75,0,0.05,cmap='hot')
 
 
 
 
 
 
-mandelbrot_image(-2.0,0.5,-1.25,1.25,cmap='hot')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
